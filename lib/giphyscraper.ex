3c7b6253633b6f5bcd2e7bdce6e7ec82dd7a300f
xander-bennett <xanderwbennett@gmail.com>
2023-12-06 15:44:50 -0700

    starting the giphyscraper module

---
+++ lib/giphyscraper.ex
@@ -0,0 +1,20 @@
+defmodule Giphyscraper do
+  @moduledoc """
+  Documentation for `Giphyscraper`.
+  """
+  use Finch
+  require Logger
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Giphyscraper.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/giphyscraper_test.exs
@@ -0,0 +1,8 @@
+defmodule GiphyscraperTest do
+  use ExUnit.Case
+  doctest Giphyscraper
+
+  test "greets the world" do
+    assert Giphyscraper.hello() == :world
+  end
+end
